ff019a990a0ab68c59817dac7d143444dbd64567
Cameron Kleiman <camtheman256@gmail.com>
2021-04-03 18:07:32 -0500

    Remove unneeded config vars

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,8 +7,6 @@ module.exports = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'MIT Dormcon',
