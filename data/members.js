528999a66d4b228d2602a70e692243529b5d135c
Cameron Kleiman <camtheman256@gmail.com>
2021-04-06 21:26:49 -0500

    officers and dorm presidents

---
+++ data/members.js
@@ -0,0 +1,68 @@
+export default members = [
+    {
+        dorm: "Baker",
+        president: "Madison Wong & Edenna Chen",
+        kerbs: ["madiwong", "edenna"],
+        url: "https://baker.mit.edu/"
+    },
+    {
+        dorm: "Burton-Conner",
+        president: "Sarah Aaronson",
+        kerbs: ["sarahaa"],
+        url: "https://bc.mit.edu/"
+    },
+    {
+        dorm: "East Campus",
+        president: "Brendan Vaughan",
+        kerbs: ["bvaughan"],
+        url: "https://ec.mit.edu/"
+    },
+    {
+        dorm: "MacGregor",
+        president: "Shruti Ravikumar",
+        kerbs: ["shrutir"],
+        url: "http://macgregor.mit.edu/"
+    },
+    {
+        dorm: "Maseeh Hall",
+        president: "Octavio J Vega",
+        kerbs: ["ovega84"],
+        url: "https://maseeh.mit.edu/"
+    },
+    {
+        dorm: "McCormick",
+        president: "Adina Golden",
+        kerbs: ["adinag"],
+        url: "https://mccormick.mit.edu/"
+    },
+    {
+        dorm: "New House",
+        president: "Duha Syar",
+        kerbs: ["duhasyar"],
+        url: "https://newhouse.mit.edu/"
+    },
+    {
+        dorm: "Next House",
+        president: "Meghana Vemulapalli",
+        kerbs: ["meghanav"],
+        url: "https://next.mit.edu/"
+    },
+    {
+        dorm: "Random Hall",
+        president: "Ret X Wang",
+        kerbs: ["mxwang"],
+        url: "http://www.mit.edu/~random-hall/"
+    },
+    {
+        dorm: "Simmons Hall",
+        president: "Shaida Nishat",
+        kerbs: ["snishat"],
+        url: "http://simmons.mit.edu/"
+    },
+    {
+        dorm: "West Garage",
+        president: "Emily Larson",
+        kerbs: ["ellarson"],
+        url: "https://studentlife.mit.edu/housing/undergraduate-housing/renovation-renewal-new-construction-projects/new-vassar-founders-group"
+    },
+];
---
+++ data/officers.js
@@ -0,0 +1,107 @@
+export default officers = [
+    {
+        position: "President",
+        name: "Sarah Edwards",
+        affiliation: "New House",
+        year: 2021,
+        kerb: "edwardss"
+    },
+    {
+        position: "Vice President",
+        name: "Shuli Jones",
+        affiliation: "East Campus",
+        year: 2022,
+        kerb: "jonsh"
+    },
+    {
+        position: "Treasurer",
+        name: "Faraz Masroor",
+        affiliation: "Next House",
+        year: 2021,
+        kerb: "fmasroor"
+    },
+    {
+        position: "Secretary",
+        name: "Anna Bair",
+        affiliation: "MacGregor",
+        year: 2023,
+        kerb: "bairanna"
+    },
+    {
+        position: "i3/RAC Chair",
+        name: "Mitali Chowdhury",
+        affiliation: "East Campus",
+        year: 2024,
+        kerb: "mitalic"
+    },
+    {
+        position: "REX Chair",
+        name: "Sunmee Choi",
+        affiliation: "Next House",
+        year: 2024,
+        kerb: "sunchoi"
+    },
+    {
+        position: "REX Chair",
+        name: "Denzel Segbefia",
+        affiliation: "Burton-Conner",
+        year: 2024,
+        kerb: "denzel"
+    },
+    {
+        position: "Housing Chair",
+        name: "Emily Caragay",
+        affiliation: "East Campus",
+        year: 2022,
+        kerb: "caragay"
+    },
+    {
+        position: "Housing Chair",
+        name: "Mohamed Mohamed",
+        affiliation: "",
+        year: 2022,
+        kerb: "mohamedm"
+    },
+    {
+        position: "Judcomm Chair",
+        name: "Jordan Wilke",
+        affiliation: "Simmons",
+        year: 2023,
+        kerb: "wilke18"
+    },
+    {
+        position: "Risk Management Chair",
+        name: "Angela J Su",
+        affiliation: "MacGregor",
+        year: 2023,
+        kerb: "asu"
+    },
+    {
+        position: "Risk Management Chair",
+        name: "Shayna Ahteck",
+        affiliation: "McCormick",
+        year: 2023,
+        kerb: "asahteck"
+    },
+    {
+        position: "Dining Chair",
+        name: "Duha Syar",
+        affiliation: "New House",
+        year: 2023,
+        kerb: "duhasyar"
+    },
+    {
+        position: "Dining Chair",
+        name: "Shaida Nishat",
+        affiliation: "Simmons",
+        year: 2022,
+        kerb: "nishat"
+    },
+    {
+        position: "🌎 Tech Chair",
+        name: "Cameron Kleiman",
+        affiliation: "Burton-Conner",
+        year: 2024,
+        kerb: "ckleiman"
+    },
+];
