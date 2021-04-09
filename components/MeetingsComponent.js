f5625696e9a5cf457b001ea71eda889edefa981c
Cameron Kleiman <camtheman256@gmail.com>
2021-04-08 20:13:09 -0500

    Fix spacing on meetings

--- components/MeetingsComponent.js
+++ components/MeetingsComponent.js
@@ -4,10 +4,10 @@ export default function MeetingsComponent({ meetings }) {
     return (
         <ul>
             {meetings.map((m, idx) => (
-                <li key={idx}>
-                    <b>{m.name}</b>&ensp;
-                    {m.minutes && (<a href={require('@site/static' + m.minutes).default} target="_blank" rel="noopener"><span className="badge badge--primary">📝 Minutes</span></a>)}
-                    <br />
+                <li key={idx} style={{marginBottom: '10px'}}>
+                    <div><b>{m.name}</b>&ensp;
+                        {m.minutes && (<a href={require('@site/static' + m.minutes).default} target="_blank" rel="noopener"><span className="badge badge--primary">📝 Minutes</span></a>)}
+                    </div>
                     <small style={{color: 'gray'}}>@ {m.location}</small>
                 </li>
             ))}
