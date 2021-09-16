67ca1a85053e5ff29fd5462e0948d4ab8a2110c5
Cameron Kleiman <camtheman256@gmail.com>
2021-09-16 18:55:37 -0400

    add fa21 meetings

--- data/meetings.js
+++ data/meetings.js
@@ -1,4 +1,29 @@
 export const meetings = {
     year: "Fall 2021",
-    list: []
+    list: [
+        {
+            name: "Thursday, September 16, 2021 at 7:00pm",
+            location: "East Campus"
+        },
+        {
+            name: "Thursday, September 30, 2021 at 7:00pm",
+            location: "New House"
+        },
+        {
+            name: "Thursday, October 14, 2021 at 7:00pm",
+            location: "Baker House"
+        },
+        {
+            name: "Thursday, October 28, 2021 at 7:00pm",
+            location: "Next House"
+        },
+        {
+            name: "Thursday, November 18, 2021 at 7:00pm",
+            location: "McCormick Hall"
+        },
+        {
+            name: "Thursday, December 2, 2021 at 7:00pm",
+            location: "Random Hall"
+        },
+    ]
 };
