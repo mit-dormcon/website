d57762f8c21556dfffcd18a9c3d4098d6613b05a
Cameron Kleiman <camtheman256@gmail.com>
2021-09-30 20:38:52 -0400

    Change wording to deemphasize minutes.

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ function FeatureRow() {
         <p>
           <ul>
             <li><Link to="/contact">Contact DormCon for help or to leave feedback.</Link></li>
-            <li><Link to="/about/meetings">Access meeting minutes and schedule.</Link></li>
+            <li><Link to="/about/meetings">View the meeting schedule.</Link></li>
           </ul> 
         </p>
       ),
