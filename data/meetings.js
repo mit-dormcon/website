ae10afd955cc55b7ff87862529096e44654ca897
Cameron Kleiman <camtheman256@gmail.com>
2022-02-16 20:27:45 -0500

    Fix meetings to be 6:30

--- data/meetings.js
+++ data/meetings.js
@@ -2,27 +2,27 @@ export const meetings = {
     year: "Spring 2022",
     list: [
         {
-            name: "Thursday, February 10, 2022 at 7:00pm",
+            name: "Thursday, February 10, 2022 at 6:30pm",
             location: "Maseeh"
         },
         {
-            name: "Thursday, February 24, 2022 at 7:00pm",
+            name: "Thursday, February 24, 2022 at 6:30pm",
             location: "Simmons"
         },
         {
-            name: "Thursday, March 10, 2022 at 7:00pm",
+            name: "Thursday, March 10, 2022 at 6:30pm",
             location: "Random"
         },
         {
-            name: "Thursday, March 31, 2022 at 7:00pm",
+            name: "Thursday, March 31, 2022 at 6:30pm",
             location: "MacGregor"
         },
         {
-            name: "Thursday, April 21, 2022 at 7:00pm",
+            name: "Thursday, April 21, 2022 at 6:30pm",
             location: "McCormick"
         },
         {
-            name: "Thursday, May 5, 2022 at 7:00pm",
+            name: "Thursday, May 5, 2022 at 6:30pm",
             location: "UA Senate Chambers"
         },
     ]
