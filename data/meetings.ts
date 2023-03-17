408c2e1ce067dc82283b98a28ca0809339b746d4
Cameron Kleiman <camtheman256@gmail.com>
2023-03-16 20:28:24 -0400

    update minutes

--- data/meetings.ts
+++ data/meetings.ts
@@ -1,23 +1,24 @@
 import { MeetingSchedule } from "./types";
 
+const minutesFolder = "https://web-cert.mit.edu/dormcon/cert_minutes/";
+
 export const meetings: MeetingSchedule = {
     year: "Spring 2023",
     list: [
         {
             name: "Thursday, February 16th, 2023 at 7pm",
             location: "Baker",
-            minutesLink:
-                "https://web-cert.mit.edu/dormcon/cert_minutes/2023-02-16.pdf",
+            minutesLink: minutesFolder + "2023-02-16.pdf",
         },
         {
             name: "Thursday, March 2nd, 2023 at 7pm",
             location: "McCormick",
-            minutesLink:
-                "https://web-cert.mit.edu/dormcon/cert_minutes/2023-03-02.pdf",
+            minutesLink: minutesFolder + "2023-03-02.pdf",
         },
         {
             name: "Thursday, March 16th, 2023 at 7pm",
             location: "Simmons",
+            minutesLink: minutesFolder + "2023-03-16.pdf",
         },
         {
             name: "Thursday, April 6th, 2023 at 7pm",
