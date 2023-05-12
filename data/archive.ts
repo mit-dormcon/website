377d5bcddbe0cc2edb571910d0db577001525a98
Cameron Kleiman <camtheman256@gmail.com>
2023-05-11 23:10:21 -0400

    add meetings to archive

--- data/archive.ts
+++ data/archive.ts
@@ -1,5 +1,7 @@
 import { MeetingSchedule, Term } from "./types";
 
+const minutesFolder = "https://web-cert.mit.edu/dormcon/cert_minutes/";
+
 const meetings: MeetingSchedule[] = [
     {
         year: "Spring 2021",
@@ -151,6 +153,41 @@ const meetings: MeetingSchedule[] = [
                 location: "New Vassar",
                 minutesLink:
                     "https://web.mit.edu/dormcon/cert_minutes/2022-12-01.pdf",
+            },
+        ],
+    },
+    {
+        year: "Spring 2023",
+        list: [
+            {
+                name: "Thursday, February 16th, 2023 at 7pm",
+                location: "Baker",
+                minutesLink: minutesFolder + "2023-02-16.pdf",
+            },
+            {
+                name: "Thursday, March 2nd, 2023 at 7pm",
+                location: "McCormick",
+                minutesLink: minutesFolder + "2023-03-02.pdf",
+            },
+            {
+                name: "Thursday, March 16th, 2023 at 7pm",
+                location: "Simmons",
+                minutesLink: minutesFolder + "2023-03-16.pdf",
+            },
+            {
+                name: "Thursday, April 6th, 2023 at 7pm",
+                location: "Random",
+                minutesLink: minutesFolder + "2023-04-06.pdf",
+            },
+            {
+                name: "Thursday, April 27th, 2023 at 7pm",
+                location: "MacGregor",
+                minutesLink: minutesFolder + "2023-04-27.pdf",
+            },
+            {
+                name: "Thursday, May 11th, 2023 at 7pm",
+                location: "On Campus (56-114)",
+                minutesLink: minutesFolder + "2023-05-11.pdf",
             },
         ],
     },
