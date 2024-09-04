b7954ee677172ee4dfa5d483fdd9d78fd657d9a7
Gabriel Rodríguez <rgabriel@mit.edu>
2024-09-03 22:47:09 -0400

    Update tentative schedule

--- data/archive.ts
+++ data/archive.ts
@@ -214,6 +214,43 @@ const meetings: MeetingSchedule[] = [
         gcalLink:
             "https://calendar.google.com/calendar/u/0?cid=NjMxYzZiMjI4Zjg0Y2QzNmVhMjhlYzU4M2JlZmE5OGEwMWRlODIwY2MwMzNjMzI4ZDU5ZDYzYjA1MjM2NjM5NUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t",
     },
+    {
+        year: "Spring 2024",
+        list: [
+            {
+                name: "Thursday, February 15th, 2024 at 7:00pm",
+                location: "Simmons",
+                minutesLink: minutesFolder + "2024-02-15.pdf",
+            },
+            {
+                name: "Thursday, February 29th, 2024 at 7:00pm",
+                location: "East Campus (Stud Space)",
+                minutesLink: minutesFolder + "2024-02-29.pdf",
+            },
+            {
+                name: "Thursday, March 14th, 2024 at 7:00pm",
+                location: "MacGregor",
+                minutesLink: minutesFolder + "2024-03-14.pdf",
+            },
+            {
+                name: "Thursday, April 4th, 2024 at 7:00pm",
+                location: "Baker",
+                minutesLink: minutesFolder + "2024-04-04.pdf",
+            },
+            {
+                name: "Thursday, April 25th, 2024 at 7:00pm",
+                location: "New House",
+                minutesLink: minutesFolder + "2024-04-25.pdf",
+            },
+            {
+                name: "Thursday, May 9th, 2024 at 7:00pm",
+                location: "1-190",
+                minutesLink: minutesFolder + "2024-05-09.pdf",
+            },
+        ],
+        gcalLink:
+            "https://calendar.google.com/calendar/u/0?cid=NjMxYzZiMjI4Zjg0Y2QzNmVhMjhlYzU4M2JlZmE5OGEwMWRlODIwY2MwMzNjMzI4ZDU5ZDYzYjA1MjM2NjM5NUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t",
+    },
 ];
 
 const officers: Term[] = [
