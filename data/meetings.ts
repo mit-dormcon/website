597552f36f1a0d7a113d48704f672b57bbfa0b15
Gabriel Rodríguez <rgabriel@mit.edu>
2024-11-21 23:26:35 -0500

    add concord market GBM

--- data/meetings.ts
+++ data/meetings.ts
@@ -33,6 +33,7 @@ export const meetings: MeetingSchedule = {
         {
             name: "Chat with Concord Market: Nov 21 @ 7:30 pm",
             location: "9-255",
+            minutesLink: minutesFolder + "2024-11-21.pdf",
         },
         {
             name: "Thursday, December 5th, 2024 at 7:30pm",
