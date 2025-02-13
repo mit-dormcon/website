af8a97cdad2933220bfa2e0da6269ed96c26f905
Gabriel Rodríguez <rgabriel@mit.edu>
2025-02-13 17:06:55 -0500

    Archive last semester

--- data/archive.ts
+++ data/archive.ts
@@ -251,6 +251,48 @@ const meetings: MeetingSchedule[] = [
         gcalLink:
             "https://calendar.google.com/calendar/u/0?cid=NjMxYzZiMjI4Zjg0Y2QzNmVhMjhlYzU4M2JlZmE5OGEwMWRlODIwY2MwMzNjMzI4ZDU5ZDYzYjA1MjM2NjM5NUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t",
     },
+    {
+        year: "Fall 2024",
+        list: [
+            {
+                name: "Thursday, September 12th, 2024 at 7:30pm",
+                location: "Simmons",
+                minutesLink: minutesFolder + "2024-09-12.pdf",
+            },
+            {
+                name: "Thursday, September 26th, 2024 at 7:30pm",
+                location: "New House",
+                minutesLink: minutesFolder + "2024-09-26.pdf",
+            },
+            {
+                name: "Thursday, October 10th, 2024 at 7:30pm",
+                location: "New Vassar",
+                minutesLink: minutesFolder + "2024-10-10.pdf",
+            },
+            {
+                name: "Thursday, October 24th, 2024 at 7:30pm",
+                location: "Next House",
+                minutesLink: minutesFolder + "2024-10-24.pdf",
+            },
+            {
+                name: "Thursday, November 7th, 2024 at 7:30pm",
+                location: "Random",
+                minutesLink: minutesFolder + "2024-11-07.pdf",
+            },
+            {
+                name: "Chat with Concord Market: Nov 21 @ 7:30 pm",
+                location: "9-255",
+                minutesLink: minutesFolder + "2024-11-21.pdf",
+            },
+            {
+                name: "Thursday, December 5th, 2024 at 7:30pm",
+                location: "McCormick",
+                minutesLink: minutesFolder + "2024-12-05.pdf",
+            },
+        ],
+        // gcalLink:
+        //     "https://calendar.google.com/calendar/u/0?cid=NjMxYzZiMjI4Zjg0Y2QzNmVhMjhlYzU4M2JlZmE5OGEwMWRlODIwY2MwMzNjMzI4ZDU5ZDYzYjA1MjM2NjM5NUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t",
+    },
 ];
 
 const officers: Term[] = [
