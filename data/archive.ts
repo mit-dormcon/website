c0b02d699b0ce5b9c2b341aae2a69106bafbc551
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2025-05-23 15:16:09 -0400

    format

--- data/archive.ts
+++ data/archive.ts
@@ -9,8 +9,16 @@ const meetings: MeetingSchedule[] = [
     {
         year: "Spring 2025",
         list: [
-            generateMeetingSchedule("Simmons", new Date(2025, 1, 13, 19, 0), true),
-            generateMeetingSchedule("Maseeh", new Date(2025, 1, 27, 19, 0), true),
+            generateMeetingSchedule(
+                "Simmons",
+                new Date(2025, 1, 13, 19, 0),
+                true,
+            ),
+            generateMeetingSchedule(
+                "Maseeh",
+                new Date(2025, 1, 27, 19, 0),
+                true,
+            ),
             generateMeetingSchedule(
                 "MacGregor",
                 new Date(2025, 2, 13, 19, 0),
