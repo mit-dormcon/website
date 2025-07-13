a0bac9962b2e024709c44b4f7dea8552789d8b34
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2025-07-13 18:36:21 -0400

    use fullscreen api

--- components/t-rex/EventfulEmbed.tsx
+++ components/t-rex/EventfulEmbed.tsx
@@ -1,23 +1,27 @@
-import Link from "@docusaurus/Link";
 import styles from "./EventfulEmbed.module.css";
+import { useRef } from "react";
 
 const EVENTFUL_URL = new URL("https://scidev5.github.io/eventful");
 const EVENTFUL_SCHEDULE_ID = "rex";
 EVENTFUL_URL.searchParams.append("event", EVENTFUL_SCHEDULE_ID);
 
 export function EventfulEmbed() {
+    const iframeRef = useRef<HTMLIFrameElement>(null);
+
     return (
         <div>
-            <Link
-                className={styles.fullscreen_link}
-                target="_self"
-                to={EVENTFUL_URL.toString()}
+            <button
+                className={"button button--link " + styles.fullscreen_link}
+                onClick={() => {
+                    void iframeRef.current?.requestFullscreen?.();
+                }}
             >
                 fullscreen <span>&#x26F6;</span>
-            </Link>
+            </button>
             <iframe
                 title="REX Events - Eventful"
                 className={styles.embed}
+                ref={iframeRef}
                 src={EVENTFUL_URL.toString() + "&is_embed"}
             />
         </div>
