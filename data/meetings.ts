36c5dd72e07e7aece0be0513a2b4f1bddf8e0ac3
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2025-07-22 15:31:18 -0400

    update meeting dates + logic

--- data/meetings.ts
+++ data/meetings.ts
@@ -1,17 +1,27 @@
 import type { Meeting, MeetingSchedule } from "./types";
 
-import { Temporal, Intl } from '@js-temporal/polyfill';
+import { Temporal, Intl } from "@js-temporal/polyfill";
 // Date.prototype.toTemporalInstant = toTemporalInstant;
 
 export const minutesFolder = "https://web-cert.mit.edu/dormcon/cert_minutes/";
 
 export const meetings: MeetingSchedule = {
     year: "Fall 2025",
-    list: [],
+    list: [
+        generateMeetingSchedule("TBD", "2025-09-11", false),
+        generateMeetingSchedule("TBD", "2025-09-25", false),
+        generateMeetingSchedule("TBD", "2025-10-09", false),
+        generateMeetingSchedule("TBD", "2025-10-23", false),
+        generateMeetingSchedule("TBD", "2025-11-06", false),
+        generateMeetingSchedule("TBD", "2025-11-20", false),
+        generateMeetingSchedule("TBD", "2025-12-04", false),
+    ],
     gcalLink: "",
 };
 
-function generateName(date: Temporal.PlainDateTime | Temporal.PlainDate): string {
+function generateName(
+    date: Temporal.PlainDateTime | Temporal.PlainDate,
+): string {
     const formatter = new Intl.DateTimeFormat("en-US", {
         weekday: "long",
         month: "long",
@@ -24,13 +34,14 @@ function generateName(date: Temporal.PlainDateTime | Temporal.PlainDate): string
     return formatter.format(date);
 }
 
-function generateMinutesUrl(date: Temporal.PlainDateTime | Temporal.PlainDate): string {
+function generateMinutesUrl(
+    date: Temporal.PlainDateTime | Temporal.PlainDate,
+): string {
     const year = date.year;
     const month = String(date.month).padStart(2, "0");
     const day = String(date.day).padStart(2, "0");
     return `${minutesFolder}${year}-${month}-${day}.pdf`;
 }
-
 
 // Of course, you can make it manually if you want a custom description like
 // "Chat with Concord Market"
@@ -40,14 +51,13 @@ export function generateMeetingSchedule(
     minutesUploaded = true,
 ): Meeting {
     if (typeof date === "string") {
+        const dateObj = Temporal.PlainDate.from(date);
         try {
-            date = Temporal.PlainDateTime.from(date);
+            const time = Temporal.PlainTime.from(date); // Check if time is present
+            date = dateObj.toPlainDateTime(time);
         } catch {
-            try {
-                date = Temporal.PlainDate.from(date);
-            } catch {
-                throw new Error(`Invalid date string: ${date.toString()}`);
-            }
+            // no time is provided, just use the date
+            date = dateObj;
         }
     }
 
