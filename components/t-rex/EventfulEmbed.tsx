adbfcf38e7a8d7f77d0b6fd6de38a71a8500fb79
SciDev5 <skhuber0@gmail.com>
2025-07-26 13:15:03 -0400

    un-break ssr on /rex/timeline

--- components/t-rex/EventfulEmbed.tsx
+++ components/t-rex/EventfulEmbed.tsx
@@ -9,21 +9,20 @@ EVENTFUL_URL.searchParams.append("trex", "https://rex.mit.edu/api.json");
 export function EventfulEmbed() {
     const iframeRef = useRef<HTMLIFrameElement>(null);
 
-    const is_ios = navigator.userAgent.includes("WebKit");
-
     return (
         <div>
             <Link
                 href={EVENTFUL_URL.toString()}
                 className={"button button--link " + styles.fullscreen_link}
                 onClick={(e) => {
+                    const is_ios = navigator.userAgent.includes("WebKit");
                     if (!is_ios) {
                         e.preventDefault()
                         void iframeRef.current?.requestFullscreen?.();
                     }
                 }}
             >
-                {is_ios ? "view in new tab" : "fullscreen"} <span>&#x26F6;</span>
+                {"fullscreen"} <span>&#x26F6;</span>
             </Link>
             <iframe
                 title="REX Events - Eventful"
