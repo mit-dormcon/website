8aca152fb486fced1826cff8325f45d161b1d7f0
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2026-02-08 14:07:53 -0500

    fix date

--- data/meetings.ts
+++ data/meetings.ts
@@ -9,7 +9,7 @@ export const meetings: MeetingSchedule = {
         generateMeetingSchedule("Burton-Conner", "2026-02-26 19:30", false),
         generateMeetingSchedule("MacGregor", "2026-03-12 19:30", false),
         generateMeetingSchedule("Maseeh", "2026-04-02 19:30", false),
-        generateMeetingSchedule("McCormick", "2026-05-23 19:30", false),
+        generateMeetingSchedule("McCormick", "2026-04-23 19:30", false),
         generateMeetingSchedule("TBD (Elections!)", "2026-05-07 19:30", false),
     ],
 };
