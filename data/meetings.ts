be52f20ba53e60a602c6a44648976b459bfea78d
camilaepierce <camilaepierce@gmail.com>
2026-04-02 21:49:20 -0400

    updated minutes

--- data/meetings.ts
+++ data/meetings.ts
@@ -19,7 +19,7 @@ export const meetings: MeetingSchedule = {
         generateMeetingSchedule("Baker", "2026-02-12 19:30"),
         generateMeetingSchedule("Burton-Conner", "2026-02-26 19:30"),
         generateMeetingSchedule("MacGregor", "2026-03-12 19:30"),
-        generateMeetingSchedule("Maseeh", "2026-04-02 19:30", false),
+        generateMeetingSchedule("Maseeh", "2026-04-02 19:30"),
         generateMeetingSchedule("McCormick", "2026-04-23 19:30", false),
         generateMeetingSchedule("1-190", "2026-05-07 19:30", false),
     ],
